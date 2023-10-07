d293424fa7366df7bf46f3e4eb34421388ec1ee6
齊尼曼 <97031067+chienniman@users.noreply.github.com>
2023-10-07 21:04:04 +0800

    v1

---
+++ laravel/database/migrations/2023_09_23_062431_add_columns_to_ratings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('ratings', function (Blueprint $table) {
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('book_id');
+            $table->unsignedInteger('rating');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('ratings', function (Blueprint $table) {
+            $table->dropColumn('user_id');
+            $table->dropColumn('book_id');
+            $table->dropColumn('rating');
+        });
+    }
+};
---
+++ laravel/routes/api.php
@@ -0,0 +1,23 @@
+<?php
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\RatingController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::post('register', [AuthController::class,'register']);
+Route::post('login',  [AuthController::class,'login']);
+Route::post('logout', [AuthController::class,'logout']);
+Route::get('user', [AuthController::class,'getAuthUser']);
+Route::apiResource('books', BookController::class);
+Route::post('books/{book}/ratings', [RatingController::class,'store'])->middleware('auth:api');
